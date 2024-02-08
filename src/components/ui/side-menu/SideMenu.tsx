8de8e5b29e62324a43d670d4f52e8170d716a62b
Ale <duartemattias4@gmail.com>
2024-02-07 22:17:40 -0400

    adding profile data in sidemenu

--- src/components/ui/side-menu/SideMenu.tsx
+++ src/components/ui/side-menu/SideMenu.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom"
 import { sidemenuOptions } from '../../../lib/constants';
 import Logo from "../../../assets/logo.svg";
+import { Button } from "@nextui-org/react";
 
 export const SideMenu = () => {
 
@@ -14,6 +15,7 @@ export const SideMenu = () => {
                 <img className="max-w-[50px]" src={ Logo } alt="Inventario" />
                 Inventario
             </div>
+
 
             <ul className="space-y-4">
                 {
@@ -29,6 +31,15 @@ export const SideMenu = () => {
                     ))
                 }
             </ul>
+
+            <div className="flex-1"></div>
+            
+            <div className="shadow-lg bg-stone-900 px-4 py-6 rounded-2xl text-center">
+                <img className="mx-auto max-w-14 mb-4" src="https://lh3.googleusercontent.com/a/ACg8ocK2GAvSNuwN-zRMJkMVv8UPMuwaDZVyBGHyPR-pU4ei1S4=s96-c-rg-br100"/>
+                <h3>Innova Code</h3>
+                <p className="text-sm">admin@innovacode.online</p>
+                <Button className="mt-4" variant="light" color="danger" fullWidth>Cerrar Sesion</Button>
+            </div>
 
 
 
